9251987ebf10f7461003db93589c7388305c6bcb
damianomondaini <damiano.mondaini@gmail.com>
2022-08-25 09:14:29 +0200

    [DONE] Added CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors')
+
+// CORS
+app.use(cors())
+app.options('*', cors())
+
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
