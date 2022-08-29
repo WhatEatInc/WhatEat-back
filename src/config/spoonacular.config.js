e48ccba3b395c1bd77109417413f0a81830849db
Soaperz <crjk.smkr@gmail.com>
2022-08-29 14:59:14 +0200

    [FIX] fixed syntax

--- src/config/spoonacular.config.js
+++ src/config/spoonacular.config.js
@@ -1,4 +1,4 @@
-allergens = [ "Dairy", "Egg", "Sesame", "Sulfite", "Gluten", 
+allergen = [ "Dairy", "Egg", "Sesame", "Sulfite", "Gluten", 
               "Grain", "Shellfish", "Tree Nut", "Peanut", "Seafood", 
               "Soy", "Wheat" ];
 
@@ -8,10 +8,16 @@ particularity = ["Gluten Free", "Ketogenic", "Vegan",
 cookType = ["African", "American", "British", "Irish", "Korean", "Middle Eastern",
             "Thai", "Cajun", "Caribbean", "Chinese", "Italian", "Latin American", "Nordic",
             "Vietnamese", "Eastern European", "European", "French", "Japanase", "Mediterranean",
-            "Southern", "German", "Greek", "Indian", "Jewish", "Mexican", "Spanish" ]
+            "Southern", "German", "Greek", "Indian", "Jewish", "Mexican", "Spanish" ];
+
+duration =  {"0" : "Short",
+            "1" : "Medium",
+            "2" : "Long"};
+
 
 module.exports = {
-    Allergens: allergens,
-    Particularity : particularity,
-    CookType: cookType
+    allergen: allergen,
+    particularity : particularity,
+    cookType: cookType,
+    duration : duration
     }
