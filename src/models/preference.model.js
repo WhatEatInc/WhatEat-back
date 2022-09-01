5af22fff311b401cb366c3ef218eb65c1e13a190
Soaperz <crjk.smkr@gmail.com>
2022-09-01 12:38:13 +0200

    [WIP] implemented real calls to spoonacular based on user prefs

--- src/models/preference.model.js
+++ src/models/preference.model.js
@@ -3,20 +3,23 @@ const mongoose = require('mongoose')
 const preferenceSchema = new mongoose.Schema({
     allergens: {
         type: Map,
-        of: String
+        of: String,
+        default: {} 
     },
     particularities: {
         type: Map,
-        of: String
+        of: String,
+        default: {} 
     },
     cookTypes: {
         type: Map,
-        of: String
+        of: String,
+        default: {} 
     },
-    healthy: { type: Boolean },
-    duration: { type: Number }
+    healthy: { type: Boolean,  default: false },
+    duration: { type: Number,  default: 2 }
 
-})
+}, { minimize: false })
 
 const Preference = mongoose.model('Preference', preferenceSchema)
 
