8f2198edc304b7986437a2899fc87569b1bba415
MasterZeus97 <thibault.seem@heig-vd.ch>
2022-09-01 10:59:41 +0200

    [WIP] Adding endpoint to open doc in the browser

---
+++ src/routes/v0/apiDoc.route.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const apiDocRouter = express.Router()
+var path = require('path');
+
+apiDocRouter.use(
+    '/',
+    express.static(path.resolve(__dirname + "../../../../apidoc"))
+);
